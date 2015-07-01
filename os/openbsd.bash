8aa051f052694da548c9099794347edf5546ece0
Florian Baumann <flo@noqqe.de>
2015-07-01 21:59:52 +0200

    openbsd specific

--- os/openbsd.bash
+++ os/openbsd.bash
@@ -1,1 +1,4 @@
 # put special cases for OpenBSD here
+export PKG_PATH="http://openbsd.cs.fau.de/pub/OpenBSD/$(uname -r)/packages/amd64/"
+export LC_CTYPE="en_US.UTF-8"
+export LC_MESSAGES="en_US.UTF-8"
