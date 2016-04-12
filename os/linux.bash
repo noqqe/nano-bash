84397f05904471fa9a18fb2d33ae770e4e7c8810
Florian Baumann <florian.baumann@gfk.com>
2016-04-12 17:10:33 +0200

    new version of z.sh

--- os/linux.bash
+++ os/linux.bash
@@ -1,18 +1,11 @@
 
 ### DirColors
-# enable nice dircolors from https://github.com/seebi/dircolors-solarized
-if [ "$OS" != "OpenBSD" ] && [ "$OS" != "Darwin" ]; then
-    if [ -x $(which dircolors) ] ; then
-        eval `dircolors $NANOBASH/configs/dircolors-solarized`
-    fi
+if [ -x $(which dircolors) ] ; then
+    eval `dircolors $NANOBASH/configs/dircolors-solarized`
 fi
 
-# Special case for oBSD
-if [ $OS != "OpenBSD" ] && [ $OS != "Darwin" ]; then
-	alias ls='ls -G'
-
-    # Enable colors with environment is ok with it
-    case "$-" in
-        *i* ) alias ls="ls --color" ;;
-    esac
-fi
+# Enable colors with environment is ok with it
+# does not apply to non-interactive sessions
+case "$-" in
+    *i* ) alias ls="ls --color" ;;
+esac
