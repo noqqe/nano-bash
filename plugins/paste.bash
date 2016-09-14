3e59dc98fa41586a309da8fa3d08e3f930ac0b44
Florian Baumann <flo@noqqe.de>
2016-09-14 15:08:43 +0200

    fixes for pastecat

--- plugins/paste.bash
+++ plugins/paste.bash
@@ -1,1 +1,6 @@
-alias pcat='curl -F "paste=<-" http://p.n0q.org'
+alias pcat='curl -H "Linx-Expiry: 172800" -H "Linx-Randomize: yes" -T - https://p.n0q.org/upload/'
+
+function catp {
+  curl -H "Linx-Expiry: 172800" -H "Linx-Randomize: yes" -T $1 https://p.n0q.org/upload/
+}
+
