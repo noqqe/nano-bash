7be09182911ae10b3e189d5c35777eb647264a7a
Florian Baumann <flo@noqqe.de>
2016-12-12 11:20:10 +0100

    http1.1 for paste service

--- plugins/paste.bash
+++ plugins/paste.bash
@@ -1,6 +1,6 @@
-alias pcat='curl -H "Linx-Expiry: 172800" -H "Linx-Randomize: yes" -T - https://p.n0q.org/upload/'
+alias pcat='curl --http1.1 -H "Linx-Expiry: 172800" -H "Linx-Randomize: yes" -T - https://p.n0q.org/upload/'
 
 function catp {
-  curl -H "Linx-Expiry: 172800" -H "Linx-Randomize: yes" -T $1 https://p.n0q.org/upload/
+  curl --http1.1 -H "Linx-Expiry: 172800" -H "Linx-Randomize: yes" -T $1 https://p.n0q.org/upload/
 }
 
