f9be06cf37d3c2d7241c2d770e711face90b4df7
Florian Baumann <flo@noqqe.de>
2017-02-14 17:56:12 +0100

    added osx bash completion

--- os/darwin.bash
+++ os/darwin.bash
@@ -1,1 +1,4 @@
 # put special cases for OS X here
+
+# bash completion from homebrew
+[ -f /usr/local/etc/bash_completion ] && . /usr/local/etc/bash_completion
--- os/linux.bash
+++ os/linux.bash
@@ -9,3 +9,8 @@ fi
 case "$-" in
     *i* ) alias ls="ls --color" ;;
 esac
+
+# sources /etc/bash.bashrc).
+if [ -f /etc/bash_completion ] && ! shopt -oq posix; then
+    . /etc/bash_completion
+fi
