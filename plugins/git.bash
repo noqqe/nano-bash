68a65a011eebc97f6cc864f9812e94954c88c730
Florian Baumann <florian.baumann@gfk.com>
2017-02-14 12:52:08 +0100

    modernized the git module

--- plugins/git.bash
+++ plugins/git.bash
@@ -1,73 +1,35 @@
-# Useful Functions
-function git_remote {
-  echo "Running: git remote add origin ${GIT_HOSTING}:$1.git"
-  git remote add origin $GIT_HOSTING:$1.git
-}
 
+# Useful Functions
+
+# remove missing
 function git_remove_missing_files {
   git ls-files -d -z | xargs -0 git update-index --remove
 }
 
-function git_info {
-    if [ -n "$(git symbolic-ref HEAD 2> /dev/null)" ]; then
-        # print informations
-        echo "git repo overview"
-        echo -e "-----------------\n\n"
-
-        # print all remotes and thier details
-        for remote in $(git remote show); do
-            echo $remote:
-            git remote show $remote
-            echo
-        done
-
-        # print status of working repo
-        echo "status:"
-        if [ -n "$(git status -s 2> /dev/null)" ]; then
-            git status -s
-        else
-            echo "working directory is clean"
-        fi
-
-        # print at least 5 last log entries
-        echo -e "\nlog:"
-        git log -5 --oneline
-    fi
+# easy committing
+function gcm {
+  git commit -a -m "$*"
 }
 
-function git_stats {
-# awesome work from https://github.com/esc/git-stats
-# including some modifications
+# git
+alias g='git'
 
-if [ -n "$(git symbolic-ref HEAD 2> /dev/null)" ]; then
-    echo "Number of commits per author:"
-    git --no-pager shortlog -sn --all
-    AUTHORS=$( git shortlog -sn --all | cut -f2 | cut -f1 -d' ')
-    LOGOPTS=""
-    if [ "$1" == '-w' ]; then
-        LOGOPTS="$LOGOPTS -w"
-        shift
-    fi
-    if [ "$1" == '-M' ]; then
-        LOGOPTS="$LOGOPTS -M"
-        shift
-    fi
-    if [ "$1" == '-C' ]; then
-        LOGOPTS="$LOGOPTS -C --find-copies-harder"
-        shift
-    fi
-    for a in $AUTHORS
-    do
-        echo '-------------------'
-        echo "Statistics for: $a"
-        echo -n "Number of files changed: "
-        git log $LOGOPTS --all --numstat --format="%n" --author=$a | cut -f3 | sort -iu | wc -l
-        echo -n "Number of lines added: "
-        git log $LOGOPTS --all --numstat --format="%n" --author=$a | cut -f1 | awk '{s+=$1} END {print s}'
-        echo -n "Number of lines deleted: "
-        git log $LOGOPTS --all --numstat --format="%n" --author=$a | cut -f2 | awk '{s+=$1} END {print s}'
-        echo -n "Number of merges: "
-        git log $LOGOPTS --all --merges --author=$a | grep -c '^commit'
-    done
-fi
+# Adding
+alias ga='git add'
+alias gall='git add .'
+
+# semi intelligent pushing
+function gp {
+  if [[ ! -z $1 ]]; then
+    BRANCH=$(git rev-parse --abbrev-ref HEAD)
+  else
+    BRANCH=$1
+  fi
+  git push origin $BRANCH
 }
+
+# Status
+alias gs='git status'
+alias gl='git log --oneline'
+alias gll='git log --graph --pretty=oneline --abbrev-commit'
+alias cdiff='git diff --cached'
