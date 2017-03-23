db5e798e62329777879f6821d6d25421aadb4986
Florian Baumann <flo@noqqe.de>
2017-03-23 09:12:33 +0100

    superuser plugin

---
+++ plugins/superuser.bash
@@ -0,0 +1,15 @@
+# A convinient way to get superuser on different systems for
+# me by just typing "s". As im using OpenBSD (doas) this needs to
+# be a plugin
+
+DOAS=$(which doas 2>/dev/null)
+SUDO=$(which sudo 2>/dev/null)
+
+if [[ ! -z $SUDO ]]; then
+  alias s="sudo -Es"
+fi
+
+if [[ ! -z $DOAS ]]; then
+  alias s="doas -s"
+fi
+
